9729217345c57cc5fc174186aae6a96bcbc3ad25
Adam <Rofikadamnugraha>
2024-03-30 14:23:32 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/newsapp/data/remote/retrofit/ApiService.kt
@@ -0,0 +1,11 @@
+package com.dicoding.newsapp.data.remote.retrofit
+
+import com.dicoding.newsapp.data.remote.response.NewsResponse
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("everything?q=apple")
+    suspend fun getNews(@Query("apiKey") apiKey: String): NewsResponse
+}
